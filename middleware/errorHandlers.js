478bdf9fb79f6a225d4cdb1d11464d842323cb2b
Luda Modna <ludmilamodna3@gmail.com>
2025-12-03 17:03:26 +0200

    Add errors handlers

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -3,6 +3,49 @@ module.exports.errorHandler = (err, req, res, next) => {
     return;
   }
 
+  if (err.name === 'CastError') {
+    return res.status(400).send({
+      errors: [
+        {
+          status: 400,
+          title: `Invalid ID format: ${err.value}`,
+        },
+      ],
+    });
+  }
+
+  if (err.name === 'ValidationError') {
+    return res.status(400).send({
+      errors: Object.values(err.errors).map(e => ({
+        status: 400,
+        title: e.message,
+      })),
+    });
+  }
+
+  if (err.code === 11000) {
+    return res.status(409).send({
+      errors: [
+        {
+          status: 409,
+          title: 'Duplicate value',
+          detail: err.keyValue,
+        },
+      ],
+    });
+  }
+
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).send({
+      errors: [
+        {
+          status: 400,
+          title: 'Invalid JSON format',
+        },
+      ],
+    });
+  }
+
   const status = err.status ?? 500;
 
   res.status(status).send({
